e590d960b9850afa5ddbb643f3b9a2f318ebd1ad
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-04-28 21:26:03 -0500

    Se agregaron archivos html para probar las rutas de expres en una WEB estatica

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # WEB Server
 
-Creacion de un WEB server con nodejs para desplegar web estaticas o dinamicas.
+Creacion de un WEB server con express para desplegar web estaticas o dinamicas.
+
+
+Para iniciar la aplicacion se tiene que instalar las dependendia usadas con el siguiente comando.
+
+```
+npm install
+```
+
+Para inicializar la aplicacion basta con ejecuar el comando:
+
+```
+npm start
+```
+
+ò
+
+```
+node app.js
+```
--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
+//Servir contenido estatico  las tutas se hacen con la ruta publica
+app.use(express.static('public'));
 
-app.get('/run', function (req, res) {
+//si la ruta no existe entra aqui
+/*app.get('/run', function (req, res) {
     res.send('Run')
 });
   
 app.get('*', function (req, res) {
   res.send('404 | Page not found')
-});
+});*/
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/404.html');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
