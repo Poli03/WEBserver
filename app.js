7e980eda2f9ad92cf6e5c738b20f942b1da57c1c
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-04-29 09:20:58 -0500

    Se agregaron las dos vistas faltantes en hbs y su ruta, tambien se agregaron elementos parciales, esstos usados en las diferente vistas

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const hbs = require('hbs');
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
 //Servir contenido estatico  las tutas se hacen con la ruta publica
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('home', {
+    name: 'Hipolito Mendoza',
+    title: 'Aplicacion con Node'
+  });
 })
 
 app.get('/elements', function (req, res) {
-  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    name: 'Hipolito Mendoza',
+    title: 'Aplicacion con Node'
+  });
 })
 
 app.get('/generic', function (req, res) {
-  res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    name: 'Hipolito Mendoza',
+    title: 'Aplicacion con Node'
+  });
 })
 
 app.get('*', function (req, res) {
